633e93f4e79573620cd309fc01506dbd309b4337
Milan <lamamilan3889@gmail.com>
2025-05-27 11:53:35 +0545

    search added

---
+++ src/js/research.js
@@ -0,0 +1,10 @@
+/*this is for the search bar open and close */
+function openSearch(){
+    let search = document.getElementById('srch');
+    search.style.display = 'flex';
+}
+
+function closeSearch(){
+  let search = document.getElementById('srch');
+    search.style.display = 'none';
+}
